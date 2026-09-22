1cc3a022c4848cc318a39a5ad4bc3cabe3a304dd
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-23 00:19:45 +0530

    Add Employee and Manager classes to demonstrate inheritance

---
+++ Employee.java
@@ -0,0 +1,27 @@
+class Employee {
+    String name;
+    int salary;
+
+    void display() {
+        System.out.println(name + " recieved a salary of Rs. " + salary);
+    }
+}
+
+class Manager extends Employee {
+    String dept;
+
+    void showDept() {
+        System.out.println("Department: " + dept);
+    }
+
+    public static void main(String[] args) {
+
+        // demonstration of subclass accessing elements from superclass
+        Manager ob = new Manager();
+        ob.name = "Akash";
+        ob.salary = 100000;
+        ob.dept = "Computer";
+        ob.display();
+        ob.showDept();
+    }
+}
