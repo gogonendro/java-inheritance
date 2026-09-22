db5a94cd77c43b806da0ba5ef408e2541d49a475
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-23 00:40:06 +0530

    Add Animal, Mammal, and Dog classes to demonstrate multilevel inheritance

---
+++ Animal.java
@@ -0,0 +1,36 @@
+//demonstration of multilevel inheritance
+class Animal {
+    String name;
+
+    void eat() {
+        System.out.println(name + " eats");
+    }
+}
+
+class Mammal extends Animal {
+    int legs;
+
+    void walk() {
+        System.out.println(name + " has " + legs + " legs");
+    }
+}
+
+class Dog extends Mammal {
+    String breed;
+
+    void bark() {
+        System.out.println(name + " is a " + breed + " dog");
+    }
+
+    public static void main(String[] args) {
+        Dog ob = new Dog();
+
+        ob.name = "Poltu";
+        ob.legs = 4;
+        ob.breed = "Native Indian";
+
+        ob.eat();
+        ob.walk();
+        ob.bark();
+    }
+}
