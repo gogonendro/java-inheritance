8d84d32b2f82438f41181f9ab58c72a590c36f50
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-23 23:20:17 +0530

    Add ParameterizedSuper class to demonstrate parameterized constructors in inheritance

---
+++ ParameterizedSuper.java
@@ -0,0 +1,29 @@
+//using parameterized super
+class Book {
+    String title;
+
+    Book(String t) {
+        title = t;
+    }
+}
+
+class TextBook extends Book {
+    String subject;
+
+    TextBook(String t, String s) {
+        super(t); // parameter is passed through constructor
+        subject = s;
+    }
+
+    void display() {
+        System.out.println("Book: " + title);
+        System.out.println("Subject: " + subject);
+    }
+}
+
+public class ParameterizedSuper {
+    public static void main(String[] args) {
+        TextBook ob = new TextBook("Mahabharata", "Mythology");
+        ob.display();
+    }
+}
