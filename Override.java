b6c7f9297ebea4d9b2c6da0c8902e46ac7b17191
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-23 19:49:53 +0530

    Add Override class to demonstrate method overriding in inheritance

---
+++ Override.java
@@ -0,0 +1,19 @@
+//Method overriding
+class Shape {
+    void draw() {
+        System.out.println("Drawing a shape");
+    }
+}
+
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing a circle");
+    }
+}
+
+public class Override {
+    public static void main(String[] args) {
+        Circle ob = new Circle();
+        ob.draw(); // this overrides the method in parent class
+    }
+}
