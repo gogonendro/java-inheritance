107edf1eb88829ab32a68e75c273df19ed53c84d
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-23 21:33:03 +0530

    Add SuperMethod class to demonstrate usage of 'super' in method overriding

---
+++ SuperMethod.java
@@ -0,0 +1,20 @@
+//using 'super' as method
+class Printer {
+    void print() {
+        System.out.println("Printing a document");
+    }
+}
+
+class ColourPrinter extends Printer {
+    void print() {
+        System.out.println("Printing a colour document");
+        super.print();
+    }
+}
+
+public class SuperMethod {
+    public static void main(String[] args) {
+        ColourPrinter ob = new ColourPrinter();
+        ob.print();
+    }
+}
