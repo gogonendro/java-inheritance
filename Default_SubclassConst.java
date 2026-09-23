dd57d7738b0ee56059ed9da509f53a82ef7e30f2
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-23 23:56:30 +0530

    Add Default_SubclassConst class to demonstrate default constructor behavior in subclass inheritance

---
+++ Default_SubclassConst.java
@@ -0,0 +1,21 @@
+//default constructor in subclass
+class Appliance {
+    Appliance() {
+        System.out.println("Appliance constructor called");
+    }
+}
+
+class WashingMachine extends Appliance {
+    // no constructor present in washing machine
+    // java automatically does the work of a constructor which calls super()
+    void display() {
+        System.out.println("Washing Machine is ready");
+    }
+}
+
+public class Default_SubclassConst {
+    public static void main(String[] args) {
+        WashingMachine ob = new WashingMachine();
+        ob.display();
+    }
+}
