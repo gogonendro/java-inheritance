bc5a89e43850ff56c8a4a1d241cd50cf501d9e3f
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-23 22:58:39 +0530

    Add ConstExec class to demonstrate constructor execution order in inheritance

---
+++ ConstExec.java
@@ -0,0 +1,24 @@
+// Constructor Execution Order: by using constructors in each class, we don't need to explicitly use 'super' to create a calling chain, it does it automatically
+class Device {
+    Device() {
+        System.out.println("Device constructor");
+    }
+}
+
+class Computer extends Device {
+    Computer() {
+        System.out.println("Computer constructor");
+    }
+}
+
+class Laptop extends Computer {
+    Laptop() {
+        System.out.println("Laptop constructor");
+    }
+}
+
+public class ConstExec {
+    public static void main(String[] args) {
+        new Laptop();
+    }
+}
